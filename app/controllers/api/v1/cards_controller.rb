cdb16a67b4f1c9b7df9d7ee6d0e7ccaf630e0f84
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2021-02-20 00:12:47 +0900

    [update] cards_controllerにauthenticateを追加

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::CardsController < ApplicationController
   before_action :set_card, only: [:show,:update,:destroy]
-  rescue_from StandardError,with: :error500
+  before_action :authenticate, only: [:update,:destroy]
+  rescue_from StandardError,with: :error500 
 
   def show
     render json: { status: 'SUCCESS', message: 'Loaded the post', data: @card }
@@ -31,11 +32,11 @@ class Api::V1::CardsController < ApplicationController
 
   def update
     card= Card.new(card_params)
-    if card.uid==@card.uid
+    if card.uid==@uid[0]
       if @card.update(card_params)
       render json: { status: 'SUCCESS', message: 'Updated the post', data: @card }
       else
-        render json: { status: 'SUCCESS', message: 'Not updated', data: @card.errors }
+        render json: { status: 'ERROR', message: 'Not updated', data: @card.errors }
       end
     else
       render json: {errors: [{code: '403',message: 'Forbidden'}]}
@@ -45,6 +46,19 @@ class Api::V1::CardsController < ApplicationController
   def set_card
     @card = Card.find_by(public_uid: params[:id])
   end
+
+  def authenticate
+    access_token = extract_access_token_from_header
+    @uid = access_token[0]['firebase']['identities']['twitter.com']
+  end
+
+  def extract_access_token_from_header
+    Firebase::Auth::IDTokenKeeper::IDToken.new(token_from_request_headers).verified_id_token
+  end
+
+  def token_from_request_headers 
+    request.headers['idToken']
+  end 
 
   def card_params #ここでPOSTされる中身を選択
     params.require(:card).permit(:uid,:text,:gif,:share,:display_name,:DM_id,:top,:left,:width,:height,:select)
---
+++ config/initializers/firebase_auth_id_token_keeper.rb
@@ -0,0 +1,3 @@
+Firebase::Auth::IDTokenKeeper.configure do |config|
+    config.firebase_project_id = 'newyearcards-2fbba'
+end 
